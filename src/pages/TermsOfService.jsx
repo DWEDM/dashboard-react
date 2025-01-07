9589dd30ce4d546aeb6eb51d819a237ec82396b0
denver <denver.delamasa@gmail.com>
2025-01-07 17:05:03 +0800

    0.1.3 | User table added...

--- src/pages/TermsOfService.jsx
+++ src/pages/TermsOfService.jsx
@@ -1,9 +1,107 @@
-function TermsOfService() {
+function TermsOfService({ setActiveMenu }) {
     return (
-      <div>
-        <h1>TermsOfService</h1>
-        <p>Here are the statistics of your site.</p>
-      </div>
+      <>
+        <div className="container mx-auto p-4">
+          <div className="card bg-base-200 shadow-md p-12">
+            <h1 className="text-5xl font-bold mb-4">Terms of Service</h1>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">1. Introduction</h2>
+              <p>Thank you for visiting this website (the “Site” or “website”). Please read the Terms and Conditions contained in this document carefully since any use of this site constitutes your acceptance of the Terms and Conditions set out herein. If you do not agree to these terms and conditions, you are free to not use this website.</p>
+              <p>This website is created to connect individuals to help adopt and rehome pets and strays. Changes or updates to these Terms and Conditions will be reflected herein.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">2. User Responsibilities</h2>
+              <ul className="flex flex-col gap-y-2 ml-8 list-decimal">
+                <li><strong>Personal Data:</strong> Personal Information to be provided by the user to create an account should be as accurate as possible. <a onClick={() => setActiveMenu('privacyPolicy')} className="text-primary">Privacy Policy</a> is found here.</li>
+                <li><strong>User Responsibility:</strong> Keep in mind that users are responsible for the content that they post. We are not responsible if the information posted here is incomplete or inaccurate. You agree that it is your responsibility to verify to the parties involved to clarify details.</li>
+                <li><strong>Prohibited Activities:</strong> Fraudulent posts, abusive behaviors, offensive content, and illegal activities are prohibited. Users are expected to report such content to create a safe and harmonious community on the platform.</li>
+              </ul>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">3. Content Ownership</h2>
+              <ul className="flex flex-col gap-y-2 ml-8 list-decimal">
+                <li><strong>Content Ownership:</strong> By posting or submitting any content (including, but not limited to, text, images, videos, or other materials) to this website, you acknowledge and agree that such content will be made public and that it will become the property of Paws'N Claws. You grant Paws'N Claws a perpetual, irrevocable, worldwide license to use, display, and modify the posted content for any purpose.</li>
+                <li><strong>Moderation of Content:</strong> You further acknowledge that the content you post will be accessible by other users of the platform and may be used or shared by those users. Paws'N Claws is under no obligation to keep any content private or confidential, and we reserve the right to remove or modify any content at our discretion.</li>
+              </ul>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">4. Pet Posting Guidelines</h2>
+              <p className=" ml-8"><strong>Responsibility and Platform Limitation of Liability:</strong> While we strive to maintain a safe and reliable community, we cannot guarantee the accuracy, authenticity, or legitimacy of any adoption offers or listings posted by users. Users are solely responsible for verifying the credibility of such offers and engaging in transSee more... or interSee more... at their own discretion.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">5. Adoption Process</h2>
+              <p>To begin the adoption process, the interested party must click the "Send Application for Adoption" button located under the post of the pet they wish to adopt. They will then be prompted to complete the following information:</p>
+              <ul className="flex flex-col gap-y-1 ml-8 mb-4 list-decimal">
+                <li>Adoption Code</li>
+                <li>Adopter Account</li>
+                <li>Adopter Name</li>
+                <li>Contact Information</li>
+                <li>Adopt Type (Specify whether the adopter is an individual, an organization, a shelter, etc.)</li>
+                <li>Employment Status</li>
+                <li>Social Media Links</li>
+                <li>Location of Adopter</li>
+                <li>Experience with Pets</li>
+                <li>Reason for Adopting</li>
+                <li>Number of Current Pets</li>
+                <li>Government-Issued ID for Identification</li>
+              </ul>
+              <p>Once the application is submitted, the pet’s current owner or organization will review the details before proceeding with the adoption process. This ensures that the adopter is fully prepared and qualified for the responsibility of pet adoption.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">6. Indemnification</h2>
+              <p className=" ml-8">You agree to indemnify and hold harmless Paws'N Claws, its officers, directors, employees, agents, and affiliates from and against any claims, liabilities, damages, losses, costs, and expenses (including reasonable legal fees) arising out of or related to your use of the platform, your violation of these Terms of Service, or your violation of any rights of another party.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">7. Termination</h2>
+              <h3 className="text-xl font-semibold ml-8">1. Termination of Access</h3>
+              <p className=" ml-8 mb-4">Paws'N Claws reserves the right to suspend, restrict, or terminate your access to the website and its services at any time, without prior notice, if you violate these Terms of Service or engage in activities deemed harmful to the platform or its users.</p>
+              
+              <h3 className="text-xl font-semibold ml-8">2. Grounds for Account Termination</h3>
+              <ul className="flex flex-col gap-y-2 ml-20 mb-4 list-decimal">
+                <li><strong>Misrepresentation:</strong> Posting false or misleading information about pets, adoption offers, or other users.</li>
+                <li><strong>Fraudulent Activity:</strong> Engaging in scams, deceptive practices, or other fraudulent behavior.</li>
+                <li><strong>Prohibited Content:</strong> Posting content that is defamatory, offensive, abusive, illegal, or violates the rights of others.</li>
+                <li><strong>Harassment:</strong> Engaging in threatening, harassing, or bullying behavior toward other users.</li>
+                <li><strong>Animal Welfare Violations:</strong> Promoting, participating in, or encouraging animal cruelty, illegal breeding, or other unethical practices related to animals.</li>
+                <li><strong>Unauthorized Use:</strong> Using the platform for purposes unrelated to pet adoption or rehoming, such as commercial advertising, spamming, or data scraping.</li>
+                <li><strong>Violation of Laws:</strong> Engaging in any activities on the platform that violate local, state, national, or international laws.</li>
+                <li><strong>Multiple Offenses:</strong> Repeated violations of community guidelines, even after warnings or temporary suspensions.</li>
+                <li><strong>Impersonation:</strong> Creating accounts under false identities or impersonating another individual or organization.</li>
+              </ul>
+              
+              <h3 className="text-xl font-semibold ml-8">3. Consequences of Termination</h3>
+              <p className=" ml-8">Upon termination, your account will be deactivated, and access to your data, posts, or content may be restricted or permanently removed. Paws'N Claws is not obligated to restore or reinstate accounts that have been terminated for violations of these Terms of Service.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">8. Dispute Resolution</h2>
+              <p className=" ml-8">Any disputes between you and Paws'N Claws arising from your use of the platform or these Terms of Service will first be addressed through informal resolution. If unresolved, disputes will be resolved through binding arbitration under Philippine Dispute Resolution Center, Inc. (PDRCI) rules, governed by the Philippine Laws. Claims must be brought individually, not as part of a class or collective action.</p>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">9. Miscellaneous</h2>
+              <ul className="flex flex-col gap-y-2 ml-8 list-decimal">
+                <li><strong>Severability:</strong> If any provision of these Terms of Service is determined to be invalid, illegal, or unenforceable by a court of competent jurisdiction, such provision shall be deemed severed from these Terms, and the remaining provisions shall continue in full force and effect.</li>
+                <li><strong>Entire Agreement:</strong> These Terms of Service, along with any other agreements, policies, or notices published by Paws'N Claws on the website, represent the complete and exclusive agreement between you and Paws'N Claws regarding your use of the platform.</li>
+                <li><strong>Force Majeure:</strong> Paws'N Claws shall not be held liable or responsible for any failure or delay in performance of its obligations under these Terms of Service if such failure or delay is caused by events beyond our reasonable control.</li>
+              </ul>
+            </section>
+          
+            <section className="mb-6">
+              <h2 className="text-2xl mt-6 mb-4 font-semibold">Copyright Infringement Disclaimer</h2>
+              <p>Paws'N Claws respects intellectual property rights. By posting content, you confirm that you own or have the necessary rights to use and share it. You agree to indemnify Paws'N Claws from any claims, damages, or liabilities arising from copyright infringement related to your content.</p>
+              <p>If you believe your copyright has been infringed, please contact us at <a href="mailto:ad.official.pawsnclaws@gmail.com" className="text-primary">ad.official.pawsnclaws@gmail.com</a>, and we will take appropriate action, including removal or restriction of access to the infringing content.</p>
+            </section>
+          </div>
+        </div>
+      </>
     );
   }
   
